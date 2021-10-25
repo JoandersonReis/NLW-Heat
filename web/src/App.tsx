84a04ee1f569395e50d1f992b9523cef991c2694
Joanderson Reis <joandersonreis470@gmail.com>
2021-10-25 20:51:37 -0300

    Created Web and finished design messages page

---
+++ web/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import styles from "./App.module.scss"
+import { LoginBox } from './components/LoginBox'
+import { MessageList } from './components/MessageList'
+
+export function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <main className={styles.contentWrapper}>
+      <MessageList />
+      <LoginBox />
+    </main>
+  )
+}
